38e803d4bf2a0eebca8c0d35f1db20580cc3b716
Denis <exinova.informes@gmail.com>
2025-06-16 22:44:25 -0500

    creacion de menu principal y llamado de funciones aritmeticas

---
+++ principal.py
@@ -0,0 +1,12 @@
+import operaciones_aritmeticas as oa
+import operaciones_logicas as ol
+
+# Definicion de inputs
+a=5
+b=4
+# llamado de funciones
+
+suma = oa.suma(a,b)
+resta = oa.resta(a,b)
+multiplicar = oa.multiplicar(a,b)
+dividir = oa.division(a,b)
