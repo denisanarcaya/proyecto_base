9684bbfafba847b8d33a55d90dfa23cac8244fcd
Denis <exinova.informes@gmail.com>
2025-06-16 22:20:50 -0500

    deficion de funciones de las operaciones basicas

---
+++ operaciones_aritmeticas.py
@@ -0,0 +1,13 @@
+def suma(a,b):
+    print(f"El resultado de la suma de {a} + {b} es {a+b}")
+    return a+b
+
+def resta(a,b):
+    print(f"El resultado de la resta de {a} - {b} es {a-b}")
+    return a-b
+
+def multiplicar(a,b):
+    print(f"El resultado de la multiplicación de {a} * {b} es {a*b}")
+    return a*b
+def division(a,b):
+    print(f"El resultado de la division de {a} / {b} es {a/b}")
